dd3d44e5a64a63cb0992cf7a9f288fea623d70ee
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-02 13:25:30 +0700

    mencoba beberapa object dan class yang diexport

--- coba.js
+++ coba.js
@@ -2,4 +2,30 @@ function cetakNama(nama) {
   return `Hello, nama saya ${nama}`;
 }
 
-module.exports = cetakNama;
+const PI = 3.14;
+
+const mahasiswa = {
+  nama: 'joko',
+  umur: 20,
+  cetakMhs() {
+    return `Halo, nama saya ${this.nama} dan saya ${this.umur} tahun`;
+  },
+};
+
+class Orang {
+  constructor() {
+    console.log('Objek orang yang telah dibuat!');
+  }
+}
+
+// module.exports.cetakNama = cetakNama;
+// module.exports.PI = PI;
+// module.exports.mahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+module.exports = {
+  cetakNama: cetakNama,
+  PI: PI,
+  mahasiswa: mahasiswa,
+  Orang: Orang,
+};
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const cetakNama = require('./coba');
+//const nama = 'chaerul anwar';
+//cont cetakNama = (nama) => `Hi, nama saya ${nama}`;
+//console.log(cetakaNama(nama));
 
-//console.log('Hello WPU');
-console.log(cetakNama('Chaerul'));
+//const fs = require('fs');//core module
+//const cetakNama = require('./coba'); //local module
+//const moment = require('moment'); //third party module/npm module/node modules
+
+//console.log('Hello WPU');p
+
+//const cetakNama = require('./coba');
+//const PI = require('./coba');
+const coba = require('./coba');
+
+console.log(coba.PI, coba.mahasiswa.cetakMhs(), new coba.Orang());
