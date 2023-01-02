e7f0ea74523c19bfe6864f4a56f0407ce13e7415
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-02 15:15:09 +0700

    mencoba write dengan gaya aycncronus

--- 7/app.js
+++ 7/app.js
@@ -4,8 +4,12 @@
 
 const fs = require('fs');
 
-try {
-  fs.writeFileSync('data/test.txt', 'Hello world secara syncronus');
-} catch (e) {
+// try {
+//   fs.writeFileSync('data/test.txt', 'Hello world secara syncronus');
+// } catch (e) {
+//   console.log(e);
+// }
+
+fs.writeFile('data/test.txt', 'Hello world secara Asynchronus', (e) => {
   console.log(e);
-}
+});
