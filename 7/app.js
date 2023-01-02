a80eb90a6aa44607bc38b514c5c88c960153f01c
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-02 15:22:50 +0700

    cara membaca file secara syncronus

--- 7/app.js
+++ 7/app.js
@@ -4,12 +4,20 @@
 
 const fs = require('fs');
 
+//menuliskan string ke file (synchonus)
+
 // try {
 //   fs.writeFileSync('data/test.txt', 'Hello world secara syncronus');
 // } catch (e) {
 //   console.log(e);
 // }
 
-fs.writeFile('data/test.txt', 'Hello world secara Asynchronus', (e) => {
-  console.log(e);
-});
+//menuliskan string ke file(asynchronus)
+
+// fs.writeFile('data/test.txt', 'Hello world secara Asynchronus', (e) => {
+//   console.log(e);
+// });
+
+//membaca isi file (syncronus)
+const data = fs.readFileSync('data/test.txt', 'utf-8');
+console.log(data);
