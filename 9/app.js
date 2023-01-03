402ca7c71fec2a922c63723975f64d53cf56895c
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-03 13:03:52 +0700

    mempersiapkan file yang dibtuhkann

---
+++ 9/app.js
@@ -0,0 +1,50 @@
+//core module
+
+//file system
+
+const fs = require('fs');
+
+//menuliskan string ke file (synchonus)
+
+// try {
+//   fs.writeFileSync('data/test.txt', 'Hello world secara syncronus');
+// } catch (e) {
+//   console.log(e);
+// }
+
+//menuliskan string ke file(asynchronus)
+
+// fs.writeFile('data/test.txt', 'Hello world secara Asynchronus', (e) => {
+//   console.log(e);
+// });
+
+//membaca isi file (syncronus)
+// const data = fs.readFileSync('data/test.txt', 'utf-8');
+// console.log(data);
+
+//membaca file secara asyncronus
+// fs.readFile('data/test.txt', 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+//core module readline
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question('masukan nama anda:', (nama) => {
+  rl.question('Masukan no HP anda : ', (noHP) => {
+    const contact = { nama, noHP };
+    const file = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+
+    contacts.push(contact);
+
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    console.log('Terimakasih sudah memasukan data.');
+    rl.close();
+  });
+});
