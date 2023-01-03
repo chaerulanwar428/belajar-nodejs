ae2e2e9948de7508e449021dca01a2a722459003
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-03 11:57:54 +0700

    mencoba chalk

--- 8/app.js
+++ 8/app.js
@@ -1,5 +1,12 @@
 const validator = require('validator');
+const chalk = require('chalk');
 
 // console.log(validator.isEmail('chaeerul@gmail.com'));
 // console.log(validator.isMobilePhone('08231313131','id-ID'));
-console.log(validator.isNumeric('10054542'));
+// console.log(validator.isNumeric('10054542'));
+
+// console.log(chalk.bgBlue.black('Hello wolrd!'));
+const nama = 'Chaerul';
+const pesan = chalk`lorem, ipsum tahak {bgRed.black lik hsjai} lalain kiki {bgGreen.black efej kaka} poin. Nama saya : ${nama}`;
+
+console.log(pesan);
