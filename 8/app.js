0796afb65331f14564170ebece6c098f840c316b
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-03 11:21:29 +0700

    mencoba beberpa validator

---
+++ 8/app.js
@@ -0,0 +1,5 @@
+const validator = require('validator');
+
+// console.log(validator.isEmail('chaeerul@gmail.com'));
+// console.log(validator.isMobilePhone('08231313131','id-ID'));
+console.log(validator.isNumeric('10054542'));
